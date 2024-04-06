91603036643cc55ebb9b0cc798c9e2696bc64c38
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-06 23:11:38 +0530

    if-else statment practice question solved in JAVASCRIPT

---
+++ JAVASCRIPT - Practical Exercise/Day-02-Conditional Statement/script.js
@@ -0,0 +1,9 @@
+alert("This is an invalid link")
+
+confirm("Do you want to proceed")
+
+let name = prompt("please enter your username","Ankita tribhuvan")
+ console.log(name);
+
+
+
---
+++ JAVASCRIPT - Practical Exercise/Day-03-If-else Statement/script.js
@@ -0,0 +1,22 @@
+// challenge time : 
+//Write a program that works out whether if a given year is a leap year or not ?//
+// A normal year has 365 days, leap years have 366, with an extra day in february. //
+
+let year = 2020;
+
+if(year % 4 === 0){
+    if(year % 100 === 0){
+        if(year % 400 === 0 ){
+
+console.log("The year" + year + "is a leap year ");
+        }else{
+            console.log(" The year " + year + " is not a leap year ");
+        }
+    }else{
+            console.log(" The year " + year + " is a leap year ");
+    }    
+}else{  
+            console.log(" The year " + year + " is not a leap year ");
+        }
+    
+
