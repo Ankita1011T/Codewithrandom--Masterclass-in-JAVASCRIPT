1bed03ae3fcde8f17ef27c26db12c37cc5d5f5b9
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-07 22:07:47 +0530

    update

---
+++ JAVASCRIPT - Practical Exercise/Day-03-If-else Statement/exm1.js
@@ -0,0 +1,9 @@
+// What is truthy and falsy value in javascript:
+// we have total 5 falsy values in javascript:
+// 0,"null","undefined","nan","", falsy is false
+
+if(score = 10){
+    console.log("OMG,we loss the game");
+}else{
+    console.log("Yay,we won the game")
+}
--- JAVASCRIPT - Practical Exercise/Day-03-If-else Statement/script.js
+++ JAVASCRIPT - Practical Exercise/Day-03-If-else Statement/script.js
@@ -2,8 +2,8 @@
 //Write a program that works out whether if a given year is a leap year or not ?//
 // A normal year has 365 days, leap years have 366, with an extra day in february. //
 
-let year = 2020;
-
+let year = 2024;
+debugger;
 if(year % 4 === 0){
     if(year % 100 === 0){
         if(year % 400 === 0 ){
