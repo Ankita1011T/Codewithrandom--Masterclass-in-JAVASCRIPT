ed839cbdea188b2d1f4ddcb4a537749a07084228
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-09 22:43:07 +0530

    Funcations

---
+++ JAVASCRIPT - Practical Exercise/Day-04-Functions statements/script.js
@@ -0,0 +1,30 @@
+function calculator(num1, num2, operator) {
+    let result;  
+    switch (operator) {
+        case  "+" :
+        return num1 + num2;
+        
+        case "-" :
+        result = num1 - num2;
+        return result;
+
+        case "*" :
+            result = num1 * num2;
+            return result;
+
+        case "/":
+            if(num2 === 0){
+                return "0 is not allowed";
+            }else{
+                result = num1 / num2;
+                return result;
+            }
+
+        default:
+            return 'no operators found';
+    }
+ }
+console.log(calculator(10, 15,'+'));
+console.log(calculator(10, 15,'-'));
+console.log(calculator(10, 0,'/'));
+console.log(calculator(10, 15,'*'));
